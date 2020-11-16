6658040292f05595f725b7649d7c5abe3f3e6d76
Kayla Snyder <ksnyder@bowdoin.edu>
2020-11-16 18:56:51 -0500

    init

--- dialogue_classifier.py
+++ dialogue_classifier.py
@@ -20,8 +20,14 @@ def get_raw_training_data(filename):
     return training_data
 
 
+def organize_raw_training_data(raw_training_data, stemmer):
+
+    for element in raw_training_data:
+
+
 def preprocess_words(words, stemmer):
-    """ Stems each word in the words list and returns a list of these word stems without duplicates."""
+    """ Stems each word in the words list and returns a 
+    list of these word stems without duplicates."""
 
     word_stems = ()
     preprocessed_words = []
@@ -29,21 +35,34 @@ def preprocess_words(words, stemmer):
     for word in words:
         stemmed_word = stemmer.stem(word)
         word_stems.add(stemmed_word)
-
+    
     for stem in word_stems:
-        new_words_list.append(stem)
+        preprocessed_words.append(stem)
     
-    return pre_processed_words
+    return preprocessed_words
 
+
+def create_training_data(words, stems, classes, documents, stemmer):
+    training_data = []
+    output = []
+
+
+
+    return training_data, output
     
 
+def sigmoid(z):
+    """Returns the basic sigmoid formula for z"""
+    denominator = 1 + np.exp(-z)
+    formula = 1/denominator
+    return formula
 
 
 def main():
     stemmer = LancasterStemmer()
 
     raw_training_data = get_raw_training_data('dialogue_data.csv')
-
+    words, classes, documents = organize_raw_training_data(raw_training_data, stemmer)
 
 if __name__ == ""__main__"":
     main()  
